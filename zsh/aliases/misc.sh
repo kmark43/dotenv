8a8c9062a749011d2cca478c8111c8c19bd836d1
Kyler Markland <kyler.markland@yahoo.com>
2026-03-27 15:33:53 -0700

    $1

---
+++ zsh/aliases/misc.sh
@@ -0,0 +1,25 @@
+
+# alias gwip="[[ '$(git rev-parse --abbrev-ref HEAD)' != 'main' ]] && git add -A && git commit -m 'wip' && git push -u origin HEAD"
+alias gs="git status -sb"
+alias gundo="git reset --soft HEAD~1"
+alias gucommit="git add -A && git commit --amend --no-edit"
+alias gl="git log --oneline --graph --decorate -20"
+
+# commit-push
+function compush () {
+    git add -A && git commit -m "$1" && git push -u origin HEAD
+}
+
+# git reset to main
+# function grmain () {
+#     git fetch --quiet
+#     git checkout main
+
+#     if [ -n "$(git rev-list @{u}..HEAD 2>/dev/null)" ]; then
+#         echo "There are unpushed commits, exiting"
+#         return 1
+#     fi
+
+#     git reset --hard origin/main
+# }
+
